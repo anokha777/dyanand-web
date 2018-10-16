ee5780c90c6d30c5736d4e1b6d9b2a9dd8a70e36
anokha777 <anokha777@gmail.com>
2018-10-16 14:01:37 +0530

    carousel issue in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerCarouselComponent } from './banner-carousel/banner-carousel.component';
+import { SuccessInfoBannerComponent } from './success-info-banner/success-info-banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BannerCarouselComponent
+    BannerCarouselComponent,
+    SuccessInfoBannerComponent
   ],
   imports: [
     BrowserModule
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isMainmenuExpanded = false;
+  isSubmenuAboutExpanded = false;
+  isSubmenuAcademicsExpanded = false;
+  isSubmenuCampusLifeExpanded = false;
   constructor() { }
 
   ngOnInit() {
@@ -19,6 +22,48 @@ export class HeaderComponent implements OnInit {
       shand[0].style.display = 'block';
     } else {
       shand[0].style.display = 'none';
+    }
+  }
+
+  submenuExpandFunction(submenuName: string) {
+    const shand = document.getElementsByClassName('dropdown') as HTMLCollectionOf<HTMLElement>;
+    if (submenuName === 'About') {
+      this.isSubmenuAboutExpanded = !this.isSubmenuAboutExpanded;
+      if (this.isSubmenuAboutExpanded) {
+        shand[0].style.display = 'block';
+        shand[1].style.display = 'none';
+        shand[2].style.display = 'none';
+        shand[0].style.width = '100%';
+      } else {
+        shand[0].style.display = 'none';
+        shand[1].style.display = 'none';
+        shand[2].style.display = 'none';
+      }
+    } if (submenuName === 'Academics') {
+        this.isSubmenuAcademicsExpanded = !this.isSubmenuAcademicsExpanded;
+        if (this.isSubmenuAcademicsExpanded) {
+          shand[0].style.display = 'none';
+          shand[1].style.display = 'block';
+          shand[2].style.display = 'none';
+          shand[1].style.width = '100%';
+        } else {
+          shand[0].style.display = 'none';
+          shand[1].style.display = 'none';
+          shand[2].style.display = 'none';
+        }
+
+    } if (submenuName === 'Campus Life') {
+      this.isSubmenuCampusLifeExpanded = !this.isSubmenuCampusLifeExpanded;
+        if (this.isSubmenuCampusLifeExpanded) {
+          shand[0].style.display = 'none';
+          shand[1].style.display = 'none';
+          shand[2].style.display = 'block';
+          shand[2].style.width = '100%';
+        } else {
+          shand[0].style.display = 'none';
+          shand[1].style.display = 'none';
+          shand[2].style.display = 'none';
+        }
     }
   }
 
